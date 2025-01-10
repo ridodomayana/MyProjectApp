91249afa7e77603f78a8dfc09282ba92bc94e190
Rido <ridoventures@gmail.com>
2025-01-10 09:13:14 -0600

    Update security.tf

--- Terraform Scripts/Jenkins_Server/security.tf
+++ Terraform Scripts/Jenkins_Server/security.tf
@@ -1,3 +1,48 @@
+# Security Group
+resource "aws_security_group" "rhel9_sg" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH from anywhere; restrict as needed
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP traffic
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTPS traffic
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
+  }
+
+  tags = {
+    Name = "RHEL9-SecurityGroup"
+  }
+}
+
+
+
+
+
+
+
+
+
 # Create Security Group - SSH Traffic and other ports
 resource "aws_security_group" "web-traffic" {
   name = "My_Security_Group1"
