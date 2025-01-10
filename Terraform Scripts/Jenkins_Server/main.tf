4fd76ca3fcc8bf6db86b6634e68d0fcef326cdb3
Rido <ridoventures@gmail.com>
2025-01-10 07:16:18 -0600

    Update main.tf

--- Terraform Scripts/Jenkins_Server/main.tf
+++ Terraform Scripts/Jenkins_Server/main.tf
@@ -8,3 +8,19 @@ resource "aws_instance" "JenkinsServer" {
     "Name" = "New_Jenkin-Server"
 }
 }
+
+
+
+
+####### Amazon Linux 2 ############
+resource "aws_instance" "JenkinsServer" {
+  ami                    = data.aws_ami.amazonlinux2.id
+  instance_type          = var.my_instance_type
+  key_name               = var.my_key
+  vpc_security_group_ids = [aws_security_group.web-traffic.id]
+
+  tags = {
+    "Name" = "Jenkins-Server"
+  }
+}
+
