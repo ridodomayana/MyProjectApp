deb68f0093ff04faff3b51b8a1518f2bc9c5461b
Rido <ridoventures@gmail.com>
2025-01-10 09:14:18 -0600

    Update variables.tf

--- Terraform Scripts/Jenkins_Server/variables.tf
+++ Terraform Scripts/Jenkins_Server/variables.tf
@@ -1,3 +1,36 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  default     = "us-east-2"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the Subnet"
+  default     = "10.0.1.0/24"
+}
+
+variable "instance_type" {
+  description = "Instance type for the RHEL 9 server"
+  default     = "t2.micro"
+}
+
+variable "key_pair_name" {
+  description = "Name of the AWS key pair to use for SSH access"
+  default     = "terraform" # Replace with your actual key pair name
+}
+
+
+
+
+
+
+
+
+
 variable "region" {
   type    = string
   default = "us-east-2"
