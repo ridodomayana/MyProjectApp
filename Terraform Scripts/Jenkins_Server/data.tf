8bdd75e2f52bd913abf2368c98b94d777f1d381d
Rido <ridoventures@gmail.com>
2025-01-10 07:14:20 -0600

    Update data.tf

--- Terraform Scripts/Jenkins_Server/data.tf
+++ Terraform Scripts/Jenkins_Server/data.tf
@@ -16,3 +16,22 @@ data "aws_ami" "rhel_9" {
     values = ["hvm:ebs-ssd"]
   }
 }
+
+
+
+######-----AMAZON LINUX 2--------------####
+data "aws_ami" "amazonlinux2" {
+  most_recent = true
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+}
+~
+
