6f64c37276fdf4b29b72c5983383c115638949bb
Rido <ridoventures@gmail.com>
2025-01-10 06:37:36 -0600

    Update Jenkins_Install.sh

--- Terraform Scripts/Jenkins_Install.sh
+++ Terraform Scripts/Jenkins_Install.sh
@@ -7,3 +7,25 @@ sudo yum install java-17-amazon-corretto -y
 sudo yum install jenkins -y
 
 
+############------Install Maven & Set Home Path for Java and Maven-----------##############
+#####-- find / -name java-17*-------#################
+#####-- Modify the .bash_profile file-----(source .bash_profile) to rebooth #################
+
+# .bash_profile
+
+# Get the aliases and functions
+if [ -f ~/.bashrc ]; then
+        . ~/.bashrc
+fi
+
+JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto.x86_64
+M2_HOME=/opt/maven
+M2=/opt/maven/bin
+
+# User specific environment and startup programs
+
+PATH=$PATH:$HOME/bin:$JAVA_HOME:$M2_HOME:$M2
+
+export PATH
+
+
