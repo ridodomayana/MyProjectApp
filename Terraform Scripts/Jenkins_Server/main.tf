fd1f147bbce49ba686d8631c9ad7469eb3cd8f2d
Rido <ridoventures@gmail.com>
2025-01-10 06:58:57 -0600

    Update main.tf

--- Terraform Scripts/Jenkins_Server/main.tf
+++ Terraform Scripts/Jenkins_Server/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "JenkinsServer" {
-  ami                    = data.aws_ami.amazonlinux2.id
+  ami                    = data.aws_ami.rhel8_server.id
   instance_type          = var.my_instance_type
   key_name               = var.my_key
   vpc_security_group_ids = [aws_security_group.web-traffic.id]
