c57a252ba0800409b7597b7f86baf96061f69fbb
Rido <ridoventures@gmail.com>
2025-01-11 04:24:02 -0600

    Update Jenkins_Install.sh

--- Terraform Scripts/Jenkins_Install.sh
+++ Terraform Scripts/Jenkins_Install.sh
@@ -6,6 +6,35 @@ sudo yum upgrade
 sudo yum install java-17-amazon-corretto -y
 sudo yum install jenkins -y
 
+#----------------Install Jenkins-----------------------------
+# Become a root
+sudo su -
+
+# Jenkins repo is added to yum.repos.d
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+# Import key from Jenkins
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+# Install Java-11
+amazon-linux-extras install java-openjdk17 -y
+
+# Install Jenkins
+yum install jenkins -y
+
+#------Start Jenkins------
+# Become a root, no need to execute if you are alread root.
+sudo su -
+
+# You can enable the Jenkins service to start at boot with the command:
+systemctl enable jenkins
+
+# You can start the Jenkins service with the command:
+systemctl start jenkins
+
+# You can check the status of the Jenkins service using the command:
+systemctl status jenkins
+
 
 ############------Install Maven & Set Home Path for Java and Maven-----------##############
 #####-- find / -name java-17*-------#################
