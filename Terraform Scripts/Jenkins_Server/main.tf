dffa62ae93d2c48c741366c4536729699d9ce1ff
Rido <ridoventures@gmail.com>
2025-01-11 04:27:58 -0600

    Update main.tf

--- Terraform Scripts/Jenkins_Server/main.tf
+++ Terraform Scripts/Jenkins_Server/main.tf
@@ -94,3 +94,14 @@ resource "aws_instance" "JenkinsServer" {
   }
 }
 
+######----Ubuntu-----########
+resource "aws_instance" "Ubuntu_Server" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.my_instance_type
+  key_name               = var.my_key
+  vpc_security_group_ids = [aws_security_group.web-traffic.id]
+
+  tags = {
+    "Name" = "Ubuntu-Jenkins-Server"
+  }
+}
