fd31eb302fd4b69065ed79c7bf0b4841f89d887d
Rido <ridoventures@gmail.com>
2025-01-11 08:08:28 -0600

    Update Ansible.sh

--- Terraform Scripts/Ansible.sh
+++ Terraform Scripts/Ansible.sh
@@ -42,8 +42,8 @@ change ownership to ansadmin (sudo chown ansadmin:ansadmin docker)
 Add post build action
 Send over ssh
 select ansible server
-webapp/target/*.war
-remove prefix --- webapp/target
+webapps/target/*.war
+remove prefix --- webapps/target
 remote directory --- //opt//docker
 
 Download docker inside ansadmin
@@ -56,8 +56,47 @@ restart the server.
 
 ------Create DockerFile---
 vi Dockerfile
+FROM tomcat:latest
+RUN cp -R /usr/local/tomcat/webapps.dist/* /usr/local/tomcat/webapps
+COPY ./*.war /usr/local/tomcat/webapps/register.war
+
+-----Create Ansible Playbook for Docker Task ---
+if config to copy ansadmin ip address
+
+sudo vi/etc/ansible/host
+
+[ansible]
+-input Ansible private IP
+
+ssh-copy-id *Private IP of the Ansible-Server*
+-------------------------------
+Create Manifest file 
+vi register-ci.yml
+- hosts: ansible
+tasks:
+- name: create docker image
+  command: docker build -t register:latest .
+  args:
+   chdir: /opt/docker
+
+- name: create tag to push image onto dockerhub
+  command: docker tag register:latest rido4good/register:latest
+
+- name: push docker image
+  command: docker push rido4good/register:latest
+
+--------------------------------------------------
+ansible-playbook register-ci.yml --check
+------------------------
+docker login -u rido4good
+-----------------------
+run playbook to test
+
+#######################---CREATE CI JOB----##########################
+create new Jenkins job, Register-CI
 
 
+https://www.youtube.com/watch?v=NKUOSc9pCfk
 
 
 
