019cb974a1afae3fbaad3a75af7406e5a73beecb
Rido <ridoventures@gmail.com>
2025-01-23 12:41:44 -0600

    Update eks.sh

--- Terraform Scripts/eks.sh
+++ Terraform Scripts/eks.sh
@@ -35,6 +35,20 @@ eksctl create cluster --name virtualtechbox-cluster \
 --node-type t2.small \
 $ kubectl get nodes
 
+############-----------IF KUBECTL IS NOT COMPATIBLE RUNNING-----############
+cd
+mv kubectl kubectl_1.24
+mv bin bin_1.24 
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.23.15/2023-01-11/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+kubectl version
+
+Finally, update your cluster
+aws eks update-kubeconfig --region us-east-2 --name rido-cluster
+########################################################
+
+
 # Create deployment Manifest File
 Refer===https://kubernetes.io/docs/concepts/workloads/controllers/deployment/
 $ nano regapp-deployment.yml
